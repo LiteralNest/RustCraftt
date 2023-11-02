a2d641fa0168dfe5e3f4fb8f73ed95d83b35d257
Sarmatt <artabc98@gmail.com>
2023-11-02 21:10:22 +0200

    Implemented main UI

--- Assets/Scripts/Main Settings/GraphicsSettings.cs
+++ Assets/Scripts/Main Settings/GraphicsSettings.cs
@@ -5,7 +5,7 @@ public class GraphicsSettings : MonoBehaviour
     private void Start()
     {
 #if UNITY_SERVER
-        Application.targetFrameRate = 5;
+        Application.targetFrameRate = 60;
         QualitySettings.vSyncCount  = 0;
 #else
         Application.targetFrameRate = 60;
