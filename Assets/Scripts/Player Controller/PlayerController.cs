ecccfd6912510be290ff29afa4900080c839bc41
Jayash <jayashkumar5599@gmail.com>
2023-11-02 22:44:06 +0200

    Update PlayerController to handle simming logic

--- Assets/Scripts/Player Controller/PlayerController.cs
+++ Assets/Scripts/Player Controller/PlayerController.cs
@@ -20,19 +20,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField] 
     private float _runningKoef = 1.5f;
     private bool _ifRunning;
-
-    private float _currentMovingCpeed;
-    private Rigidbody _rb;
-
+    private float _currentMovingSpeed;
+    
     [Header("Swim")]
     [SerializeField] private float _swimSpeed;
-    [SerializeField] private Transform _target;
     public bool IsSwimming { get; set; }
     
+    private Rigidbody _rb;
+
+    private float _originalDrag;
+    private float _originalAngularDrag;
+    
+    private readonly float _targetDrag = 0.4f;
+    private readonly float _targetAngularDrag = 1f;
+    private readonly float _floatStrength = 0.4f;
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        _currentMovingCpeed = _movingSpeed.Value;
+        
+        _originalDrag = _rb.drag;
+        _originalAngularDrag = _rb.angularDrag;
+        _currentMovingSpeed = _movingSpeed.Value;
     }
 
     private void FixedUpdate()
@@ -40,28 +49,31 @@ public class PlayerController : MonoBehaviour
         if (IsSwimming != true)
         {
             if (_rb.useGravity != true)
+            {
                 _rb.useGravity = true;
+                _rb.drag = _originalDrag;
+                _rb.angularDrag = _originalAngularDrag;
+            }
             if (!_ifRunning)
             {
                 Move();
                 return;
             }
-            transform.Translate(Vector3.forward * _currentMovingCpeed * Time.deltaTime, Space.Self);
+            transform.Translate(Vector3.forward * _currentMovingSpeed * Time.deltaTime, Space.Self);
         }
         else
         {
-            if (_rb.useGravity) 
-                _rb.useGravity = false;
-
-            if (_move.y > 0) transform.position += _target.forward * _swimSpeed * Time.deltaTime;
-             if (_move.y < 0) transform.position -= _target.forward * _swimSpeed * Time.deltaTime;
+            if (_rb.useGravity) _rb.useGravity = false;
+            Swim();
         }
     }
 
+    #region InputMap
     public void OnMove(InputAction.CallbackContext context)
     {
         if (IsSwimming)
         {
+            IsMoving = true;
             _move = context.ReadValue<Vector2>();
         }
         else
@@ -70,9 +82,12 @@ public class PlayerController : MonoBehaviour
             _move = context.ReadValue<Vector2>();
             IsMoving = false;
         }
-      
     }
+    
 
+    #endregion
+    
+    #region Movement
     private void Move()
     {
         Vector3 movement = new Vector3(_move.x, 0f, _move.y);
@@ -80,7 +95,7 @@ public class PlayerController : MonoBehaviour
         {
             _legsAnimator.SetBool("Walking", true);
             _inHandObjectsContainer.SetWalk(true);
-            transform.Translate(movement * _currentMovingCpeed * Time.deltaTime, Space.Self);
+            transform.Translate(movement * _currentMovingSpeed * Time.deltaTime, Space.Self);
             return;
         }
 
@@ -88,23 +103,12 @@ public class PlayerController : MonoBehaviour
         _legsAnimator.SetBool("Walking", false);
     }
 
-    private void Swim()
-    {
-        Vector3 movement = new Vector3(_move.x, 0f, _move.y);
-        if (movement != Vector3.zero)
-        {
-            // Add swimming logic here
-            transform.Translate(movement * _swimSpeed * Time.deltaTime, Space.Self);
-            return;
-        }
-    }
-    
     public void StartRunning()
     {
         _inHandObjectsContainer.SetRun(true);
         _legsAnimator.SetBool("Running", true);
         _ifRunning = true;
-        _currentMovingCpeed *= _runningKoef;
+        _currentMovingSpeed *= _runningKoef;
     }
 
     public void StopRunning()
@@ -112,6 +116,36 @@ public class PlayerController : MonoBehaviour
         _inHandObjectsContainer.SetRun(false);
         _legsAnimator.SetBool("Running", false);
         _ifRunning = false;
-        _currentMovingCpeed = _movingSpeed.Value;
+        _currentMovingSpeed = _movingSpeed.Value;
     }
+    
+
+    #endregion
+
+    #region Swimming
+    private void Swim()
+    {
+        Vector3 movement = new Vector3(_move.x, 0f, _move.y);
+        if (movement != Vector3.zero)
+        {
+            _rb.drag = _targetDrag;
+            _rb.angularDrag = _targetAngularDrag;
+            transform.Translate(movement * _swimSpeed * Time.deltaTime, Space.Self);
+            return;
+        }
+    }
+
+    private void FloatUp()
+    {
+        if (IsSwimming)
+        {
+            Vector3 jumpForce = Vector3.up * _floatStrength;
+            _rb.AddForce(jumpForce, ForceMode.Force);
+        }
+    }
+    
+    
+
+    #endregion
+   
 }
