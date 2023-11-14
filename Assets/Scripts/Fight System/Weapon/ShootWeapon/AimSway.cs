281213be0927a41b92cd4638ada14a8997c2b537
Jayash <jayashkumar5599@gmail.com>
2023-11-14 11:30:43 +0200

    fixed position

--- Assets/Scripts/Fight System/Weapon/ShootWeapon/AimSway.cs
+++ Assets/Scripts/Fight System/Weapon/ShootWeapon/AimSway.cs
@@ -4,38 +4,24 @@ namespace Fight_System.Weapon.ShootWeapon
 {
     public class AimSway : MonoBehaviour
     {
-        [Header("Adjust how much the weapon sways")] [SerializeField]
-        private float _swayAmount = 1f;
+        [Header("Adjust how much the weapon sways")]
+        [SerializeField] private float _swayAmount = 1f;
 
-        [Header("Maximum sway amount")] [SerializeField]
-        private float _maxSwayAmount = 0.14f;
+        [Header("Maximum sway amount")]
+        [SerializeField] private float _maxSwayAmount = 0.05f;
 
-        [Header("Horizontal sway bounds")] [SerializeField]
-        private float _swayXBounds = 0.01f;
+        [Header("Horizontal sway bounds")]
+        [SerializeField] private float _swayXBounds = 0.01f;
 
-        [Header("Vertical sway bounds")] [SerializeField]
-        private float _swayYBounds = 0.005f;
-
-        [Header("Smoothing factor for sway")] [SerializeField]
-        private float _smoothFactor = 1f;
-
-        [Header("Adjust how much weapon rotates")] [SerializeField]
-        private float _rotationAmount = 3.5f;
-
-        [Header("Adjust how much the weapon sways")] [SerializeField]
-        private float _maxRotation = 6.0f;
-
-        [Header("Maximum rotation amount")] [SerializeField]
-        private float _returnSpeed = 4f;
+        [Header("Smoothing factor for sway")]
+        [SerializeField] private float _smoothFactor = 2f;
 
         private Vector3 _initialPosition;
-        private Quaternion _initialRotation;
         private Vector2 _previousTouchPosition;
 
         private void Start()
         {
             _initialPosition = transform.localPosition;
-            _initialRotation = transform.localRotation;
         }
 
         private void Update()
@@ -49,18 +35,9 @@ namespace Fight_System.Weapon.ShootWeapon
                 {
                     float swayX = Mathf.Clamp(-touchDeltaPosition.x * _swayAmount, -_maxSwayAmount, _maxSwayAmount) *
                                   _swayXBounds;
-                    float swayY = Mathf.Clamp(-touchDeltaPosition.y * _swayAmount, -_maxSwayAmount, _maxSwayAmount) *
-                                  _swayYBounds;
 
-                    Vector3 swayPosition = _initialPosition + new Vector3(swayX, swayY, 0f);
+                    Vector3 swayPosition = _initialPosition + new Vector3(swayX, 0f, 0f);
                     transform.localPosition = Vector3.Lerp(transform.localPosition, swayPosition,
-                        Time.deltaTime * _smoothFactor);
-
-                    float rotationX = Mathf.Clamp(-touchDeltaPosition.y * _rotationAmount, -_maxRotation, _maxRotation);
-                    float rotationY = Mathf.Clamp(touchDeltaPosition.x * _rotationAmount, -_maxRotation, _maxRotation);
-
-                    Quaternion targetRotation = Quaternion.Euler(rotationX, rotationY, 0f) * _initialRotation;
-                    transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation,
                         Time.deltaTime * _smoothFactor);
                 }
 
@@ -69,9 +46,7 @@ namespace Fight_System.Weapon.ShootWeapon
             else
             {
                 transform.localPosition =
-                    Vector3.Lerp(transform.localPosition, _initialPosition, Time.deltaTime * _returnSpeed);
-                transform.localRotation = Quaternion.Slerp(transform.localRotation, _initialRotation,
-                    Time.deltaTime * _returnSpeed);
+                    Vector3.Lerp(transform.localPosition, _initialPosition, Time.deltaTime * _smoothFactor);
             }
         }
     }
