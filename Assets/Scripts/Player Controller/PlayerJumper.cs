2a4afd5349484f5924e7e8b01b9705fd05cf5c24
Sarmatt <artabc98@gmail.com>
2024-01-08 16:21:14 +0200

    Optimization

--- Assets/Scripts/Player Controller/PlayerJumper.cs
+++ Assets/Scripts/Player Controller/PlayerJumper.cs
@@ -1,41 +1,43 @@
 using Animation_System;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
-public class PlayerJumper : MonoBehaviour
+namespace Player_Controller
 {
-    [Header("Attached Scripts")]
-    [SerializeField] private CharacterController _characterController;
-    [SerializeField] private float Gravity = -9.8f;
-    [Header("Main Params")]
-    [SerializeField] private float _jumpForce = 20f;
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerJumper : MonoBehaviour
+    {
+        [Header("Attached Scripts")]
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private float Gravity = -9.8f;
+        [Header("Main Params")]
+        [SerializeField] private float _jumpForce = 20f;
 
-    private Vector3 _velocity;
+        private Vector3 _velocity;
     
-    private void Update()
-    {
-        if (Input.GetButtonDown("Jump"))
+        private void Update()
         {
-            Jump();
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jump();
+            }
+
+            UpdateGravity();
         }
 
-        UpdateGravity();
-    }
-
-    private void Jump()
-    {
-        AnimationsManager.Singleton.SetJump();
-
-        if (_characterController.isGrounded)
+        private void Jump()
         {
-            _velocity.y = Mathf.Sqrt(_jumpForce * -2f * Gravity);
+            AnimationsManager.Singleton.SetJump();
+
+            if (_characterController.isGrounded)
+            {
+                _velocity.y = Mathf.Sqrt(_jumpForce * -2f * Gravity);
+            }
         }
-    }
 
-    private void UpdateGravity()
-    {
-        _velocity.y += Gravity * Time.deltaTime;
-
-        _characterController.Move(_velocity * Time.deltaTime);
+        private void UpdateGravity()
+        {
+            _velocity.y += Gravity * Time.deltaTime;
+            _characterController.Move(_velocity * Time.deltaTime);
+        }
     }
 }
