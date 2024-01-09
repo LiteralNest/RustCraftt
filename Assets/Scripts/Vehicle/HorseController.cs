7894c8ff3eee45298750754b5cc2907178b99174
Jayash <jayashkumar5599@gmail.com>
2024-01-09 22:19:40 +0200

    fixed horse

--- Assets/Scripts/Vehicle/HorseController.cs
+++ Assets/Scripts/Vehicle/HorseController.cs
@@ -2,18 +2,32 @@ using Player_Controller;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace Vehicle
 {
-    public class HorseController : VehicleController, IVehicleController
+    public class HorseController : VehicleController
     {
         [SerializeField] private Horse _horse;
+        [SerializeField] private float _obstacleCheckDistance = 2f;
         
-        private void FixedUpdate()
+        private void Update()
         {
             if (IsMoving)
             {
                 _horse.Move(MoveInput);
+
+                CheckForObstacle();
+            }
+        }
+        
+        private void CheckForObstacle()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, _obstacleCheckDistance, LayerMask.GetMask("Ground")))
+            {
+                
+                _horse.Jump();
             }
         }
     }
