e1fad5051d0acf86a801d51aa6f7487f389f1f92
Sarmatt <artabc98@gmail.com>
2024-01-10 17:55:51 +0200

    Added Async NetObjects Loading

---
+++ Assets/Scripts/Web/NetObjectsSpawner/NetObjectSpawner.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Web.NetObjectsSpawner
+{
+    public class NetObjectSpawner : MonoBehaviour
+    {
+        [SerializeField] private string _prefabPath = "NetObject/";
+        
+        public void GeneratePref()
+        {
+            var position = transform.position;
+            Debug.Log("Generating " + _prefabPath + " at " + position);
+            ClearChildren();
+            var prefab = Resources.Load<GameObject>(_prefabPath);
+            var instance = Instantiate(prefab, position, Quaternion.identity);
+            instance.GetComponent<NetworkObject>().Spawn();
+        }
+        
+        private void ClearChildren()
+        {
+            foreach(Transform child in transform)
+                Destroy(child.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Web/NetObjectsSpawner/NetObjectSpawnersContainer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Linq;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Web.NetObjectsSpawner
+{
+    public class NetObjectSpawnersContainer : NetworkBehaviour
+    {
+        [SerializeField] private float _delayBetweenSpawning = 0.01f;
+
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            if(!IsServer) return;
+            StartCoroutine(LoadSpawnersRoutine());
+        }
+
+        private IEnumerator LoadSpawnersRoutine()
+        {
+            var spawners = FindObjectsOfType<NetObjectSpawner>().ToList();
+            foreach (var spawner in spawners)
+            {
+                spawner.GeneratePref();
+                yield return new WaitForSeconds(_delayBetweenSpawning);
+            }
+        }
+    }
+}
