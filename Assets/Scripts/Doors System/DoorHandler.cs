7622e14be9026b7e758981fe7bd872b21e59d534
Sarmatt <artabc98@gmail.com>
2024-01-10 13:20:38 +0200

    Added Single Metal Door

--- Assets/Scripts/Doors System/DoorHandler.cs
+++ Assets/Scripts/Doors System/DoorHandler.cs
@@ -9,31 +9,46 @@ public class DoorHandler : NetworkBehaviour, ILockable
 
     private NetworkVariable<bool> _wasOpened = new();
 
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        _wasOpened.OnValueChanged += (bool prevValue, bool newValue) =>
+        {
+            Debug.Log(newValue);
+            OpenClientRpc(newValue);
+        };
+    }
+
     private void Start()
         => gameObject.tag = "Door";
 
-    [ServerRpc(RequireOwnership = false)]
-    private void SetWasOpenedServerRpc(bool value)
-    {
-        if (!IsServer) return;
-        _wasOpened.Value = value;
-    }
-    
     public void Open(int id)
     {
         if (_locker != null && !_locker.CanBeOpened(id)) return;
-        if(!_wasOpened.Value)
+        OpenServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void OpenServerRpc()
+    {
+        if (!IsServer) return;
+        _wasOpened.Value = !_wasOpened.Value;
+    }
+
+    [ClientRpc]
+    private void OpenClientRpc(bool value)
+    {
+        if (value)
             _anim.SetTrigger("Open");
         else
             _anim.SetTrigger("Close");
-        SetWasOpenedServerRpc(!_wasOpened.Value);
     }
 
     #region ILockable
 
     public void Lock(Locker locker)
         => _locker = locker;
-    
+
 
     bool ILockable.IsLocked()
         => _locker != null;
