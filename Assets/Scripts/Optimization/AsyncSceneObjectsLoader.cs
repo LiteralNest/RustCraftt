606be3091a4f928faca31f23fa766e3f4ec872fb
Sarmatt <artabc98@gmail.com>
2024-01-10 11:09:29 +0200

    Added async objects loading

---
+++ Assets/Scripts/Optimization/AsyncSceneObjectsLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+namespace Optimization
+{
+    public class AsyncSceneObjectsLoader : MonoBehaviour
+    {
+        [SerializeField] private string _objectsPath = "StartSceneObjects";
+        [SerializeField] private Transform _parrent;
+        [SerializeField] private float _waitingPerObjectTime;
+
+        private void Start()
+        =>StartCoroutine(LoadSceneObjectsAsync());
+
+        private IEnumerator LoadSceneObjectsAsync()
+        {
+            var objects = Resources.LoadAll<GameObject>(_objectsPath);
+            foreach (var prefab in objects)
+            {
+                yield return new WaitForSeconds(_waitingPerObjectTime);
+                Instantiate(prefab, _parrent);
+            }
+        }
+    }
+}
