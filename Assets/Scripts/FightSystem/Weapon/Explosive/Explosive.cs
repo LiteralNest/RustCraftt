2ec1a9be9c1e98995763362c34b9509e581b4040
Sarmatt <artabc98@gmail.com>
2024-01-12 15:05:36 +0200

    Connected Explosion

--- Assets/Scripts/FightSystem/Weapon/Explosive/Explosive.cs
+++ Assets/Scripts/FightSystem/Weapon/Explosive/Explosive.cs
@@ -5,7 +5,6 @@ namespace FightSystem.Weapon.Explosive
     public class Explosive : BaseExplosive
     {
         [SerializeField] private float _timeToExplode = 3f;
-
         private float _countdownTimer;
 
         protected override void Start()
---
+++ Assets/Scripts/InHandItems/InHandExplosive.cs
@@ -0,0 +1,41 @@
+﻿using InHandItems.InHandViewSystem;
+using Items_System.Items;
+using Multiplayer.Multiplay_Instances;
+using Player_Controller;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace InHandItems
+{
+    public class InHandExplosive : NetworkBehaviour, IViewable
+    {
+        private const string ViewName = "Weapon/View/ExplosiveView";
+        
+        [SerializeField] private float _throwForce = 10f;
+        [SerializeField] private Transform _spawnPoint;
+
+        private void Start()
+        {
+            var view = Instantiate(Resources.Load<InHandView>(ViewName));
+            view.Init(this);
+        } 
+        
+        public void TryThrow()
+        {
+            SpawnAndThrowExplosiveServerRpc(_spawnPoint.position, GetComponent<MultiplayInstanceId>().Id);
+            PlayerNetCode.Singleton.InHandObjectsContainer.SetDefaultHands();
+            InventoryHandler.singleton.RemoveActiveSlotDisplayer();
+        } 
+    
+        [ServerRpc(RequireOwnership = false)]
+        private void SpawnAndThrowExplosiveServerRpc(Vector3 position, int id)
+        {
+            if(!IsServer) return;
+            var obj = MultiplayObjectsPool.singleton.GetMultiplayInstanceIdById(id).gameObject;
+            var explosive = Instantiate(obj, position, Quaternion.identity);
+            explosive.GetComponent<NetworkObject>().Spawn(true);
+            var rb = explosive.GetComponent<Rigidbody>();
+            if (rb != null) rb.AddForce(Camera.main.transform.forward * _throwForce, ForceMode.Impulse);
+        }
+    }
+}
