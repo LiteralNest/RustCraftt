3c06652c51b6710e9f556683ed53690a50f2fb3b
Sarmatt <artabc98@gmail.com>
2024-01-24 15:31:16 +0200

    Added map scrolling

--- Assets/Scripts/Map/MapFingerScroller.cs
+++ Assets/Scripts/Map/MapFingerScroller.cs
@@ -2,56 +2,60 @@
 
 namespace Map
 {
+    [RequireComponent(typeof(Camera))]
     public class MapFingerScroller : MonoBehaviour
     {
-        private bool _isDragging = false;
-        private Vector2 _dragStartPosition;
+        [SerializeField] private Vector2 _orthographicSizeLimits = new Vector2(75, 350);
+        [SerializeField] private float _zoomSpeed = 1f;
+        [SerializeField] private float _moveSpeed = 0.5f;
+        
+        private Vector2 _cachedFingersPosition;
+
+        private Camera _camera;
+
+        private void Start()
+            => _camera = GetComponent<Camera>();
 
         private void Update()
-            => DetectFingerInput();
-
-        void DetectFingerInput()
         {
-            if (Input.touchCount > 0)
+            if (Input.touchCount == 1)
+                HandleMovement();
+            else if (Input.touchCount == 2)
+                HandleScroll();
+        }
+
+        private void HandleMovement()
+        {
+            var touch = Input.GetTouch(0);
+
+            if (touch.phase != TouchPhase.Moved) return;
+            var yOffset = touch.deltaPosition.y * _moveSpeed;
+            var xOffset = touch.deltaPosition.x * _moveSpeed;
+
+            transform.Translate(xOffset, yOffset, 0f);
+        }
+
+        private void HandleScroll()
+        {
+            var touch = Input.GetTouch(0);
+            var touch2 = Input.GetTouch(1);
+            var touchDelta = touch.position - touch2.position;
+            switch (touch.phase)
             {
-                Touch touch = Input.GetTouch(0);
+                case TouchPhase.Began:
+                    _cachedFingersPosition = touchDelta;
+                    break;
+                case TouchPhase.Moved:
+                    var deltaMagnitudeDiff = _cachedFingersPosition.magnitude - touchDelta.magnitude;
+                    var orthographicSize = _camera.orthographicSize;
+                    orthographicSize += deltaMagnitudeDiff * _zoomSpeed /* Time.deltaTime*/;
 
-                switch (touch.phase)
-                {
-                    case TouchPhase.Began:
-                        OnDragBegin(touch.position);
-                        break;
-
-                    case TouchPhase.Moved:
-                        OnDrag(touch.position);
-                        break;
-
-                    case TouchPhase.Ended:
-                        OnDragEnd();
-                        break;
-                }
+                    _cachedFingersPosition = touchDelta;
+                    _camera.orthographicSize = orthographicSize;
+                    _camera.orthographicSize =
+                        Mathf.Clamp(orthographicSize, _orthographicSizeLimits.x, _orthographicSizeLimits.y);
+                    break;
             }
-        }
-
-        void OnDragBegin(Vector2 startPosition)
-        {
-            _isDragging = true;
-            _dragStartPosition = startPosition;
-        }
-
-        void OnDrag(Vector2 currentPosition)
-        {
-            if (_isDragging)
-            {
-                Vector2 dragDelta = currentPosition - _dragStartPosition;
-                //scrollRect.normalizedPosition -= new Vector2(dragDelta.x / Screen.width, dragDelta.y / Screen.height);
-                _dragStartPosition = currentPosition;
-            }
-        }
-
-        void OnDragEnd()
-        {
-            _isDragging = false;
         }
     }
 }
