701769eb677960224cc2f04aa6e3f468bdae2887
Sarmatt <artabc98@gmail.com>
2024-01-27 01:07:15 +0200

    Fixed crossbow and bow

--- Assets/Scripts/FightSystem/Weapon/ShootWeapon/Bow.cs
+++ Assets/Scripts/FightSystem/Weapon/ShootWeapon/Bow.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using Building_System.NetWorking;
 using FightSystem.Weapon.ShootWeapon.Ammo;
 using InHandItems.InHandAnimations.Weapon;
@@ -12,12 +12,14 @@ namespace FightSystem.Weapon.ShootWeapon
         private const string ViewName = "Weapon/View/BowWeaponView";
 
         [Header("Bow")] [SerializeField] private Items_System.Items.Ammo _ammo;
-        [SerializeField] private Arrow _arrowPrefab;
         [SerializeField] private float _arrowForce;
         [SerializeField] private BowAnimator _weaponAnimator;
         [SerializeField] private Transform _ammoSpawnPoint;
+        [SerializeField] private AnimationClip _prepearingForShootClip;
         private Arrow _currentArrow;
         private Vector3 _force;
+        
+        private bool _canShoot;
 
         private int _currentAmmoCount;
 
@@ -32,6 +34,11 @@ namespace FightSystem.Weapon.ShootWeapon
         public void Attack()
         {
             if (_currentAmmoCount <= 0) return;
+            if (!_canShoot)
+            {
+                _weaponAnimator.SetIdle();
+                return;
+            }
             _weaponAnimator.SetAttack();
             ShootArrow();
             InventoryHandler.singleton.CharacterInventory.RemoveItem(_ammo.Id, 1);
@@ -40,8 +47,16 @@ namespace FightSystem.Weapon.ShootWeapon
         public void Scope()
         {
             if (!EnoughAmmo()) return;
+            StartCoroutine(WaitForScopingRoutine());
             _currentAmmoCount = 1;
             _weaponAnimator.SetScope();
+        }
+        
+        private IEnumerator WaitForScopingRoutine()
+        {
+            _canShoot = false;
+            yield return new WaitForSeconds(_prepearingForShootClip.length);
+            _canShoot = true;
         }
 
         private bool EnoughAmmo()
--- Assets/Scripts/InHandItems/InHandAnimations/Weapon/BowAnimator.cs
+++ Assets/Scripts/InHandItems/InHandAnimations/Weapon/BowAnimator.cs
@@ -4,11 +4,15 @@ namespace InHandItems.InHandAnimations.Weapon
     {
         private const string ScopeKey = "Scope";
         private const string AttackKey = "Attack";
+        private const string IdleKey = "Idle";
 
         public void SetScope()
             => PlayAnimationServerRpc(ScopeKey);
 
         public void SetAttack()
             => PlayAnimationServerRpc(AttackKey);
+        
+        public void SetIdle()
+            => PlayAnimationServerRpc(IdleKey);
     }
 }
