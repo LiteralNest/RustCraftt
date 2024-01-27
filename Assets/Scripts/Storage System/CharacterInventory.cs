62accc4f997cbb827d89781429b8eddee056e033
Sarmatt <artabc98@gmail.com>
2024-01-27 02:22:29 +0200

    Added default items when spawn

--- Assets/Scripts/Storage System/CharacterInventory.cs
+++ Assets/Scripts/Storage System/CharacterInventory.cs
@@ -1,29 +1,36 @@
+using System.Collections.Generic;
 using Events;
 using Inventory_System.Inventory_Slot_Displayers;
+using UnityEngine;
 
 namespace Storage_System
 {
     public class CharacterInventory : Storage
     {
+        [SerializeField] private List<InventoryCell> _defaultItems = new List<InventoryCell>();
+
         private QuickSlotDisplayer _activeQuickSlot;
-        
-        public  override  void OnNetworkSpawn()
+
+        public override void OnNetworkSpawn()
         {
             base.OnNetworkSpawn();
             SlotsDisplayer.DisplayCells();
+
+            foreach(var slot in _defaultItems)
+                AddItemToDesiredSlotServerRpc(slot.Item.Id, slot.Count, slot.Ammo);
             
             ItemsNetData.OnValueChanged += (oldValue, newValue) =>
             {
-                if(!IsOwner) return;
+                if (!IsOwner) return;
                 GlobalEventsContainer.InventoryDataChanged?.Invoke();
             };
         }
-        
+
         public override void Open(InventoryHandler handler)
         {
             SlotsDisplayer.DisplayCells();
         }
-        
+
         protected override void Appear()
         {
         }
@@ -40,7 +47,7 @@ namespace Storage_System
 
         public void SetActiveQuickSlot(QuickSlotDisplayer quickSlot)
         {
-            if(_activeQuickSlot != null)
+            if (_activeQuickSlot != null)
                 _activeQuickSlot.OnSlotDisabled();
             _activeQuickSlot = quickSlot;
         }
