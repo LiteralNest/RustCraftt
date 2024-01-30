5fd9d645a7811348b1ca616005f1e43b97c1a310
Sarmatt <artabc98@gmail.com>
2024-01-30 14:15:18 +0200

    Fixed Ladder

--- Assets/Scripts/Player Controller/PlayerClimber.cs
+++ Assets/Scripts/Player Controller/PlayerClimber.cs
@@ -1,85 +1,90 @@
 using System.Collections;
 using Building_System;
-using Player_Controller;
 using UI;
+using Unity.Netcode;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerJumper))]
-[RequireComponent(typeof(CharacterController))]
-public class PlayerClimber : MonoBehaviour
+namespace Player_Controller
 {
-    [SerializeField] private float _climbForce = 10.0f;
-    [SerializeField] private float _raycastDistance = 0.3f;
-    [SerializeField] private Transform _raycastOrigin;
-    
-    private CharacterController _characterController;
-    private PlayerJumper _jumper;
-
-    private Ladder _currentLadder;
-    private bool _climbButtonPressed;
-
-    private void Start()
+    [RequireComponent(typeof(PlayerJumper))]
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerClimber : NetworkBehaviour
     {
-        _characterController = GetComponent<CharacterController>();
-        _jumper = GetComponent<PlayerJumper>();
-    }
+        [SerializeField] private NetworkObject _playerNetworkObject;
+        [SerializeField] private float _climbForce = 10.0f;
+        [SerializeField] private float _raycastDistance = 0.3f;
+        [SerializeField] private Transform _raycastOrigin;
 
-    private void Update()
-    {
-        if(CharacterUIHandler.singleton == null) return;
-        bool ladderFound = LadderFound();
-        CharacterUIHandler.singleton.HandleMovingUpButton(ladderFound);
-    }
+        private CharacterController _characterController;
+        private PlayerJumper _jumper;
 
-    private void FixedUpdate()
-    {
-        if (!_climbButtonPressed) return;
-        TryClimb(_jumper.transform, _currentLadder);
-    }
+        private Ladder _currentLadder;
+        private bool _climbButtonPressed;
+
+        private void Start()
+        {
+            _characterController = GetComponent<CharacterController>();
+            _jumper = GetComponent<PlayerJumper>();
+        }
+
+        private void Update()
+        {
+            if(CharacterUIHandler.singleton == null) return;
+            if(!_playerNetworkObject.IsOwner) return;
+            bool ladderFound = LadderFound();
+            CharacterUIHandler.singleton.HandleMovingUpButton(ladderFound);
+        }
+
+        private void FixedUpdate()
+        {
+            if (!_climbButtonPressed) return;
+            TryClimb(_jumper.transform, _currentLadder);
+        }
 
 #if UNITY_EDITOR
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawRay(_raycastOrigin.position, _raycastOrigin.forward * _raycastDistance);
-    }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(_raycastOrigin.position, _raycastOrigin.forward * _raycastDistance);
+        }
 
 #endif
 
-    public void AssignClimbing(bool value)
-    {
-        if (!value)
+        public void AssignClimbing(bool value)
         {
-            if(_climbButtonPressed)
-                StartCoroutine(AssignGravityRoutine());
+            if (!value)
+            {
+                if(_climbButtonPressed)
+                    StartCoroutine(AssignGravityRoutine());
+            }
+            else
+                _jumper.SetGravity(false);
+            _climbButtonPressed = value;
         }
-        else
-            _jumper.SetGravity(false);
-        _climbButtonPressed = value;
-    }
 
-    private IEnumerator AssignGravityRoutine()
-    {
-        yield return null;
-        _jumper.SetGravity(_characterController.isGrounded || !_currentLadder);
-    }
+        private IEnumerator AssignGravityRoutine()
+        {
+            yield return null;
+            _jumper.SetGravity(_characterController.isGrounded || !_currentLadder);
+        }
 
-    private bool LadderFound()
-    {
-        _currentLadder = null;
-        if (!Physics.Raycast(_raycastOrigin.position, _raycastOrigin.forward, out RaycastHit hit, _raycastDistance)) return false;
-        if (!hit.collider.CompareTag("DamagingItem") && !hit.collider.CompareTag("Ladder")) return false;
-        if (!hit.collider.TryGetComponent(out Ladder ladder)) return false;
-        _currentLadder = ladder;
-        return true;
-    }
+        private bool LadderFound()
+        {
+            _currentLadder = null;
+            if (!Physics.Raycast(_raycastOrigin.position, _raycastOrigin.forward, out RaycastHit hit, _raycastDistance)) return false;
+            if (!hit.collider.CompareTag("DamagingItem") && !hit.collider.CompareTag("Ladder")) return false;
+            if (!hit.collider.TryGetComponent(out Ladder ladder)) return false;
+            _currentLadder = ladder;
+            return true;
+        }
 
-    private void TryClimb(Transform player, Ladder ladder)
-    {
-        if (!_currentLadder) return;
-        Vector3 climbDirection = ladder.transform.up;
-        player.position += climbDirection * _climbForce * Time.deltaTime;
-        _jumper.MoveWithLadder(climbDirection * _climbForce * Time.deltaTime);
+        private void TryClimb(Transform player, Ladder ladder)
+        {
+            if (!_currentLadder) return;
+            Vector3 climbDirection = ladder.transform.up;
+            player.position += climbDirection * _climbForce * Time.deltaTime;
+            _jumper.MoveWithLadder(climbDirection * _climbForce * Time.deltaTime);
+        }
     }
 }
