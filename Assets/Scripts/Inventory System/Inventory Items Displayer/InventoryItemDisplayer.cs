188dd62629f907bc74311c4d3bbd1e5e02e034f4
Sarmatt <artabc98@gmail.com>
2024-01-31 12:09:44 +0200

    Fixed Garage Door

--- Assets/Scripts/Inventory System/Inventory Items Displayer/InventoryItemDisplayer.cs
+++ Assets/Scripts/Inventory System/Inventory Items Displayer/InventoryItemDisplayer.cs
@@ -38,11 +38,6 @@ namespace Inventory_System.Inventory_Items_Displayer
             if (_countText != null)
                 _countText.gameObject.SetActive(true);
             transform.position = PreviousCell.transform.position;
-            // if (InventoryCell.Count == 0)
-            //     _storage.ResetItemServerRpc(PreviousCell.Index);
-            // else
-            //     _storage.SetItemServerRpc(PreviousCell.Index,
-            //         new CustomSendingInventoryDataCell(InventoryCell.Item.Id, InventoryCell.Count, InventoryCell.Hp, InventoryCell.Ammo));
             transform.SetParent(PreviousCell.transform);
             _itemIcon.raycastTarget = true;
         }
