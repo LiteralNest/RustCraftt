87a73510ba5bf6d542d709214cac5c79f8e0a117
Jayash <jayashkumar5599@gmail.com>
2024-02-01 16:36:00 +0200

    Created GlobalValues for setting and logic to manipulate them

--- Assets/Scripts/GlobalValues.cs
+++ Assets/Scripts/GlobalValues.cs
@@ -3,4 +3,10 @@ public static class GlobalValues
     public static bool CanDragInventoryItems { get; set; }
     public static bool CanLookAround { get; set; } = true;
     public static float Sensitivity { get; set; } = 1;
+    public static int GraphicsQualityIndex { get; set; }
+    public static float CameraFarDistance { get; set; } = 100f;
+    public static float CameraFOV { get; set; } = 60f;
+    public static bool EnableShadows { get; set; }
+    public static bool EnableGrass { get; set; }
+    
 }
