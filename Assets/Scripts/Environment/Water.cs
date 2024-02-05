77c06ba6434f38d9f711f864a60fd210b9a1764f
Jayash <jayashkumar5599@gmail.com>
2024-02-05 10:54:00 +0200

    Added player oxigen display stat

--- Assets/Scripts/Environment/Water.cs
+++ Assets/Scripts/Environment/Water.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using CharacterStatsSystem;
 using Player_Controller;
 using UnityEngine;
@@ -7,52 +8,47 @@ namespace Environment
 {
     public class Water : MonoBehaviour
     {
-        private float _waveHieght = 0f; //Could be used later for water
+        private float _waveHeight = 0f;
         private bool _isRestoringOxygen = false;
+        private Coroutine _oxygenCoroutine;
+        
+        private CharacterStats _characterStats;
 
-        private void Update()
+        private void OnEnable()
         {
-            if (_isRestoringOxygen)
-            {
-                CharacterStatsEventsContainer.OnCharacterStatAdded.Invoke(CharacterStatType.Oxygen, (int)(1 * Time.fixedDeltaTime));
-            }
+            CharacterStatsEventsContainer.OnCharacterStatsAssign += Init;
         }
 
+        private void Init(CharacterStats characterStats)
+        {
+            _characterStats = characterStats;
+        }
+        
         private void OnTriggerEnter(Collider other)
         {
-
             if (other.CompareTag("Player") && other.GetComponent<PlayerController>() != null)
             {
                 _isRestoringOxygen = false;
-                CharacterStatsEventsContainer.OnCharacterStatRemoved.Invoke(CharacterStatType.Oxygen, 10);
-                var move = other.GetComponent<PlayerController>();
-                move.IsSwimming = true;
-            }
+                // var move = other.GetComponent<PlayerController>();
+                // move.IsSwimming = true;
 
-        }
-
-        private void OnTriggerStay(Collider other)
-        {
-            if (other.CompareTag("Boat"))
-            {
-                var boat = other.GetComponent<Boat>();
-                if (boat != null) boat.Float(_waveHieght, true);
-            }
-
-            if (other.CompareTag("Player") && other.GetComponent<PlayerController>() != null)
-            {
-                CharacterStatsEventsContainer.OnCharacterStatRemoved.Invoke(CharacterStatType.Oxygen, (int)(1 * Time.fixedDeltaTime));
+                _oxygenCoroutine = StartCoroutine(RemoveOxygenOverTime());
             }
         }
 
         private void OnTriggerExit(Collider other)
         {
-
             if (other.CompareTag("Player") && other.GetComponent<PlayerController>() != null)
             {
                 _isRestoringOxygen = true;
-                var move = other.GetComponent<PlayerController>();
-                move.IsSwimming = false;
+                // var move = other.GetComponent<PlayerController>();
+                // move.IsSwimming = false;
+
+                if (_oxygenCoroutine != null)
+                {
+                    StopCoroutine(_oxygenCoroutine);
+                }
+                StartCoroutine(RestoreOxygenToFull());
             }
 
             if (other.CompareTag("Boat"))
@@ -60,8 +56,27 @@ namespace Environment
                 var boat = other.GetComponent<Boat>();
                 if (boat != null)
                 {
-                    boat.Float(_waveHieght, false);
+                    boat.Float(_waveHeight, false);
                 }
+            }
+        }
+
+        // Coroutine to gradually restore oxygen over time
+        private IEnumerator RemoveOxygenOverTime()
+        {
+            while (!_isRestoringOxygen)
+            {
+                yield return new WaitForSeconds(1f);
+                CharacterStatsEventsContainer.OnCharacterStatRemoved.Invoke(CharacterStatType.Oxygen, 1);
+            }
+        }
+        
+        private IEnumerator RestoreOxygenToFull()
+        {
+            while (_characterStats.Oxygen.Value < 100)
+            {
+                yield return new WaitForSeconds(1f);
+                CharacterStatsEventsContainer.OnCharacterStatAdded.Invoke(CharacterStatType.Oxygen, 1);
             }
         }
     }
