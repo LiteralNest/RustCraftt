d0d9af778ba422a110592716a31b1ca51cd68bb1
Jayash <jayashkumar5599@gmail.com>
2024-02-06 17:12:49 +0200

    Added UI server list

---
+++ Assets/Scripts/UI/ServerSelectionData.cs
@@ -0,0 +1,10 @@
+namespace UI
+{
+    public static class ServerSelectionData
+    {
+        public static string SelectedServerIp { get; set; }
+        public static int SelectedServerPort { get; set; }
+        public static string AllocateServerIpv4 { get; set; }
+        public static int AllocateServerGamePort { get; set; }
+    }
+}
