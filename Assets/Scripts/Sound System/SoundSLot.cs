c8ddf95859744d1220d58619aa92b21cf5a304bc
Sarmatt <artabc98@gmail.com>
2024-02-19 15:21:00 +0200

    Added rt sounds

--- Assets/Scripts/Sound System/SoundSLot.cs
+++ Assets/Scripts/Sound System/SoundSLot.cs
@@ -5,7 +5,6 @@ namespace Sound_System
     [System.Serializable]
     public class SoundSLot
     {
-        public int Id;
         [Range(0f, 1f)]
         public float Volume = 1f;
         public AudioClip Clip;
