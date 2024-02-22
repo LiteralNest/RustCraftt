8d40e0db53e43e328af970cb485b2d9d1f48006e
Sarmatt <artabc98@gmail.com>
2024-02-22 13:55:28 +0200

    Finished blocks stabilization system

---
+++ Assets/Scripts/BlocksStabilizationSystem/BlockSnap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace BlocksStabilizationSystem
+{
+    [RequireComponent(typeof(BoxCollider), typeof(Rigidbody))]
+    public class BlockSnap : MonoBehaviour
+    {
+        [Header("Attached Components")] [SerializeField]
+        private StabilizationBlock _stabilizationBlock;
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            var collider = GetComponent<BoxCollider>();
+            Gizmos.DrawCube(transform.position, new Vector3(collider.size.x, collider.size.y, collider.size.z));
+        }
+#endif
+
+        private void OnTriggerEnter(Collider other)
+        {
+            CheckBlockSnapEnter(other);
+            CheckGroundEnter(other);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            CheckGroundExit(other);
+        }
+
+        private void CheckBlockSnapEnter(Collider other)
+        {
+            if (!other.TryGetComponent(out BlockSnap blockSnap)) return;
+            _stabilizationBlock.AssignSnappedBlock(blockSnap._stabilizationBlock);
+        }
+
+        private void CheckGroundEnter(Collider other)
+        {
+            if (!other.CompareTag("Ground")) return;
+            _stabilizationBlock.SetIsGrounded(true);
+        }
+
+        private void CheckGroundExit(Collider other)
+        {
+            if (!other.CompareTag("Ground")) return;
+            _stabilizationBlock.SetIsGrounded(false);
+        }
+    }
+}
---
+++ Assets/Scripts/BlocksStabilizationSystem/StabilizationBlock.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlocksStabilizationSystem
+{
+    public class StabilizationBlock : MonoBehaviour
+    {
+        private List<StabilizationBlock> _stabilizationBlocks = new List<StabilizationBlock>();
+        private bool _isGrounded;
+
+        private void OnDestroy()
+        {
+            _isGrounded = false;
+            CheckStabilization();
+        }
+
+        public void SetIsGrounded(bool value)
+            => _isGrounded = value;
+
+        private void CheckStabilization()
+        {
+            if (IsGrounded(this)) return;
+            for (int i = 0; i < _stabilizationBlocks.Count; i++)
+            {
+                var block = _stabilizationBlocks[i];
+                if (block == this) return;
+                if (block == null || block.gameObject == null) continue;
+                _stabilizationBlocks.Remove(block);
+                i--;
+                Destroy(block.gameObject);
+            }
+        }
+
+        private bool IsGrounded(StabilizationBlock maskedBlock)
+        {
+            if (_isGrounded) return true;
+            foreach (var block in _stabilizationBlocks)
+                if (block != null && block != maskedBlock && block.IsGrounded(this))
+                    return true;
+            return false;
+        }
+
+        public void AssignSnappedBlock(StabilizationBlock stabilizationBlock)
+        {
+            if (_stabilizationBlocks.Contains(stabilizationBlock) || stabilizationBlock == this) return;
+            _stabilizationBlocks.Add(stabilizationBlock);
+        }
+    }
+}
